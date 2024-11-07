0aeeb0f071d8b9358d6a557ea8df7932fbf662d7
VinaQhurotuAini <220711673@students.uajy.ac.id>
2024-11-07 15:11:18 +0700

    Update

--- Tugas_Modul4_B_Vina_11673.py
+++ Tugas_Modul4_B_Vina_11673.py
@@ -51,11 +51,10 @@ if uploaded_file is not None:
     st.dataframe(input_data)
 
     # Direktori tempat penyimpanan ketiga model
-    model_directory = r'C:/Users/vina qhurotu aini/Downloads/Unsupervised Learning (Praktek)/Unsupervised Learning (Praktek)/Tugas4_B_11673'
     model_path = {
-        "AGG_model": os.path.join(model_directory, r'AGG_model.pkl'),
-        "KMeans_model": os.path.join(model_directory, r'KMeans_model.pkl'),
-        "DBSCAN_model": os.path.join(model_directory, r'DBSCAN_model.pkl')
+        "AGG_model": 'AGG_model.pkl'),
+        "KMeans_model": 'KMeans_model.pkl'),
+        "DBSCAN_model": 'DBSCAN_model.pkl')
     }
 
     # Load ketiga model ke dalam dictionary
